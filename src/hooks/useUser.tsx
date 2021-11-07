2cf5a61db2a8ca9d980e2745ff42e8c7c69ebdc9
fahad-clarifai <fahad.aziz@clarifai.com>
2021-11-07 20:27:17 +0200

    session template protected

---
+++ src/hooks/useUser.tsx
@@ -0,0 +1,29 @@
+import { useEffect } from 'react'
+
+import Router from 'next/router'
+import useSWR from 'swr'
+
+import { GetUserResponse } from 'src/lib/types'
+import { fetchGetJSON } from 'src/utils/api-helpers'
+
+export default function useUser({ redirectTo }: { redirectTo: string }) {
+  const { data, error } = useSWR<{ user: GetUserResponse }>(
+    '/api/user',
+    fetchGetJSON
+  )
+
+  useEffect(() => {
+    if (error) {
+      Router.push(redirectTo || '/login')
+    }
+  }, [error, redirectTo])
+
+  return !error && data
+    ? {
+        loading: false,
+        user: data.user,
+      }
+    : {
+        loading: true,
+      }
+}
--- src/templates/InSession.tsx
+++ src/templates/InSession.tsx
@@ -1,16 +1,34 @@
 import React from 'react'
 
+import { Box, SkeletonCircle, SkeletonText } from '@chakra-ui/react'
+
 import Sidebar from 'src/components/Sidebar'
 import UpperPannel from 'src/components/UpperPannel'
+import useUser from 'src/hooks/useUser'
 
-const InSession = ({ children }: React.PropsWithChildren<{}>) => (
-  <div className="w-screen h-screen">
-    <Sidebar />
-    <div className="right-content">
-      <UpperPannel />
-      <div className="pt-12">{children}</div>
-    </div>
-  </div>
-)
+const InSession = ({ children }: React.PropsWithChildren<{}>) => {
+  const { loading, user } = useUser({ redirectTo: '/login' })
+  console.log('user at inSession template: ', user)
+  return (
+    <>
+      {loading ? (
+        <div className="w-screen h-screen">
+          <Box padding="6" boxShadow="lg" bg="white">
+            <SkeletonCircle size="10" />
+            <SkeletonText mt="4" noOfLines={4} spacing="4" />
+          </Box>
+        </div>
+      ) : (
+        <div className="w-screen h-screen">
+          <Sidebar />
+          <div className="right-content">
+            <UpperPannel />
+            <div className="pt-12">{children}</div>
+          </div>
+        </div>
+      )}
+    </>
+  )
+}
 
 export default InSession
